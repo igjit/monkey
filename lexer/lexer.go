3cf40720a18e7121ef6da4e396dbcdf3956e4faf
igjit <igjit1@gmail.com>
2020-07-14 04:56:17 +0900

    Refactor: extract to method

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -16,13 +16,17 @@ func New(input string) *Lexer {
 }
 
 func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
+	l.ch = l.peekChar()
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 func (l *Lexer) NextToken() token.Token {
