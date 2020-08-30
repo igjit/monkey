2c73ebbdd569d16b75fe7630d3ffdd8d5e0bc693
igjit <igjit1@gmail.com>
2020-08-31 08:28:03 +0900

    Complete REPL

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/igjit/monkey/evaluator"
 	"github.com/igjit/monkey/lexer"
 	"github.com/igjit/monkey/parser"
 	"io"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
